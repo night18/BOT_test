9cbcbdc90c245423ff0f1963e719ce9c713c80dd
night18 <warrior.sp@gmail.com>
2017-01-09 15:11:03 -0500

    change global environment variables

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-// Add your requirements
-var restify = require('restify'); 
-var builder = require('botbuilder'); 
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function() 
-{
+server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
-
+  
 // Create chat bot
-var connector = new builder.ChatConnector
-({ appId: 'YourAppId', appPassword: 'YourAppPassword' }); 
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-// Create bot dialogs
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
 bot.dialog('/', function (session) {
     session.send("Hello World");
 });
