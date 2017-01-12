7886782f3ce268f3c0290dd0feccce362b57f6f7
night18 <warrior.sp@gmail.com>
2017-01-12 11:18:39 -0500

    add change name

--- app.js
+++ app.js
@@ -18,25 +18,56 @@ var connector = new builder.ChatConnector({
 });
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
+var intents = new builder.IntentDialog();
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', [
+bot.dialog('/',intents);
+
+intents.matches(/^change name/i,[
 	function(session){
 		session.beginDialog('/askName');
 	},
 	function(session, results){
-		session.send('Hello %s !', results.response);
+		session.send('OK! Change your name to %s !', session.privateConversationData.name);
+	}]);
+
+intents.onDefault( [
+	function(session, args, next){
+		if(!session.privateConversationData.name){
+			session.beginDialog('/askName');
+		}else{
+			next();
+		}
+		
+	},
+	function(session, results){
+		session.send('Hello %s !', session.privateConversationData.name);
 	}
 ]);
+
+// bot.dialog('/', [
+// 	function(session, args, next){
+// 		if(!session.privateConversationData.name){
+// 			session.beginDialog('/askName');
+// 		}else{
+// 			next();
+// 		}
+		
+// 	},
+// 	function(session, results){
+// 		session.send('Hello %s !', session.privateConversationData.name);
+// 	}
+// ]);
 
 bot.dialog('/askName',[
 	function(session){
 		builder.Prompts.text(session, 'Hi, what is your name?');
 	},
 	function(session, results){
-		session.endDialogWithResult(results);
+		session.privateConversationData.name = results.response;
+		session.endDialog();
 	}]
 )
